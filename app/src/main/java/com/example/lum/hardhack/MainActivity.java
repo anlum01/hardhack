07c8ee5af37e65f3ea40d30e24f0e38421b4247f
Andrew T. Li <andrewfourli@gmail.com>
2018-01-13 10:45:12 -0800

    read and write to the firebase database

--- app/src/main/java/com/example/lum/hardhack/MainActivity.java
+++ app/src/main/java/com/example/lum/hardhack/MainActivity.java
@@ -5,9 +5,11 @@ import android.os.Bundle;
 import android.widget.SeekBar;
 
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
-
+import com.google.firebase.database.ValueEventListener;
 
 
 import java.util.ArrayList;
@@ -33,73 +35,107 @@ public class MainActivity extends AppCompatActivity {
         light3 = (SeekBar) findViewById(R.id.light3);
 
         mDatabase = FirebaseDatabase.getInstance().getReference();
-        System.out.print(mDatabase);
-//        mDatabase.addValueEventListener(new ValueEventListener() {
-//            @Override
-//            public void onDataChange(DataSnapshot dataSnapshot) {
-//                String value = dataSnapshot.getValue(String.class);
-//                int intesity = Integer.parseInt(value);
-//
-//            }
-//
-//            @Override
-//            public void onCancelled(DatabaseError databaseError) {
-//
-//            }
-//        });
+        //System.out.print(mDatabase);
+        mDatabase.child("light1").addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                long value = dataSnapshot.getValue(Long.class);
+                light1.setProgress((int) value);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+            }
+        });
+
+        mDatabase.child("light2").addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                long value = dataSnapshot.getValue(Long.class);
+                light2.setProgress((int) value);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+            }
+        });
+
+        mDatabase.child("light3").addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                long value = dataSnapshot.getValue(Long.class);
+                light3.setProgress((int) value);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+            }
+        });
 
         light1.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
             int progressChangedValue = 0;
+            boolean gesture = false;
             @Override
             public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
                 progressChangedValue = progress;
-                // this overrides the current one in database
-                mDatabase.child("light1").setValue(progress);
             }
 
             public void onStartTrackingTouch(SeekBar seekBar) {
-                // TODO Auto-generated method stub
+                if( gesture == false) {
+                    gesture = true;
+                }
             }
 
             public void onStopTrackingTouch(SeekBar seekBar) {
-                //Toast.makeText(MainActivity.this, "Seek bar progress is :" + progressChangedValue,
-                //          Toast.LENGTH_SHORT).show();
+                if (gesture == true) {
+                    mDatabase.child("light1").setValue(progressChangedValue);
+                    gesture = false;
+                }
             }
         });
+
         light2.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
             int progressChangedValue = 0;
+            boolean gesture = false;
 
+            @Override
             public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
                 progressChangedValue = progress;
-                // this overrides the current one in database
-                mDatabase.child("light2").setValue(progress);
             }
 
             public void onStartTrackingTouch(SeekBar seekBar) {
-                // TODO Auto-generated method stub
+                if( gesture == false) {
+                    gesture = true;
+                }
             }
 
             public void onStopTrackingTouch(SeekBar seekBar) {
-                //Toast.makeText(MainActivity.this, "Seek bar progress is :" + progressChangedValue,
-                //          Toast.LENGTH_SHORT).show();
+                if (gesture == true) {
+                    mDatabase.child("light2").setValue(progressChangedValue);
+                    gesture = false;
+                }
             }
         });
+
         light3.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
             int progressChangedValue = 0;
+            boolean gesture = false;
 
             public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
                 progressChangedValue = progress;
-                // this overrides the current one in database
-                mDatabase.child("light3").setValue(progress);
             }
 
             public void onStartTrackingTouch(SeekBar seekBar) {
-                // TODO Auto-generated method stub
+                if( gesture == false) {
+                    gesture = true;
+                }
             }
 
             public void onStopTrackingTouch(SeekBar seekBar) {
-                //Toast.makeText(MainActivity.this, "Seek bar progress is :" + progressChangedValue,
-                //          Toast.LENGTH_SHORT).show();
+                if (gesture == true) {
+                    mDatabase.child("light3").setValue(progressChangedValue);
+                    gesture = false;
+                }
             }
         });
 
