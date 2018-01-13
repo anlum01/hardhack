659c513cf4bf56796cd2ab3b642691b10f0c484d
Andrew T. Li <andrewfourli@gmail.com>
2018-01-13 06:08:39 -0800

    code

--- app/src/main/java/com/example/lum/hardhack/MainActivity.java
+++ app/src/main/java/com/example/lum/hardhack/MainActivity.java
@@ -2,12 +2,111 @@ package com.example.lum.hardhack;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.SeekBar;
+
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+    private SeekBar light1;
+    private SeekBar light2;
+    private SeekBar light3;
+    // database
+//    FirebaseDatabase database;
+    private DatabaseReference mDatabase;
+    private FirebaseDatabase fire;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
+//        database = FirebaseDatabase.getInstance();
+
+
+        light1 = (SeekBar) findViewById(R.id.light1);
+        light2 = (SeekBar) findViewById(R.id.light2);
+        light3 = (SeekBar) findViewById(R.id.light3);
+
+        mDatabase = FirebaseDatabase.getInstance().getReference();
+        System.out.print(mDatabase);
+//        mDatabase.addValueEventListener(new ValueEventListener() {
+//            @Override
+//            public void onDataChange(DataSnapshot dataSnapshot) {
+//                String value = dataSnapshot.getValue(String.class);
+//                int intesity = Integer.parseInt(value);
+//
+//            }
+//
+//            @Override
+//            public void onCancelled(DatabaseError databaseError) {
+//
+//            }
+//        });
+
+        light1.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            int progressChangedValue = 0;
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                progressChangedValue = progress;
+                // this overrides the current one in database
+                mDatabase.child("light1").setValue(progress);
+            }
+
+            public void onStartTrackingTouch(SeekBar seekBar) {
+                // TODO Auto-generated method stub
+            }
+
+            public void onStopTrackingTouch(SeekBar seekBar) {
+                //Toast.makeText(MainActivity.this, "Seek bar progress is :" + progressChangedValue,
+                //          Toast.LENGTH_SHORT).show();
+            }
+        });
+        light2.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            int progressChangedValue = 0;
+
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                progressChangedValue = progress;
+                // this overrides the current one in database
+                mDatabase.child("light2").setValue(progress);
+            }
+
+            public void onStartTrackingTouch(SeekBar seekBar) {
+                // TODO Auto-generated method stub
+            }
+
+            public void onStopTrackingTouch(SeekBar seekBar) {
+                //Toast.makeText(MainActivity.this, "Seek bar progress is :" + progressChangedValue,
+                //          Toast.LENGTH_SHORT).show();
+            }
+        });
+        light3.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            int progressChangedValue = 0;
+
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                progressChangedValue = progress;
+                // this overrides the current one in database
+                mDatabase.child("light3").setValue(progress);
+            }
+
+            public void onStartTrackingTouch(SeekBar seekBar) {
+                // TODO Auto-generated method stub
+            }
+
+            public void onStopTrackingTouch(SeekBar seekBar) {
+                //Toast.makeText(MainActivity.this, "Seek bar progress is :" + progressChangedValue,
+                //          Toast.LENGTH_SHORT).show();
+            }
+        });
+
+
+
+
 }
+
+
+    }
